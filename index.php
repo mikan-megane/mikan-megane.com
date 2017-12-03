4c61ba85c59991addfa63a122c164b5e07364a2f
mikan-megane <info@mikan-megane.com>
2017-12-03 17:53:39 +0900

    やっつけ仕事感

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>mikan-megane - 準備中</title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport'
+  />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Hind+Siliguri:700|Kanit:200italic|Martel:600" rel="stylesheet" />
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+  <!-- CSS Files -->
+  <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/now-ui-kit.css" rel="stylesheet" />
+
+  <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/logo.css" rel="stylesheet" />
+  <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/brandmark.css" rel="stylesheet" />
+</head>
+
+<body class="index-page">
+  <!-- Navbar -->
+  <nav class="navbar navbar-toggleable-md bg-primary fixed-top navbar-transparent" id="demo-navbar" color-on-scroll="120">
+    <div class="container">
+      <div class="navbar-translate">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </button>
+
+        <div id="demo_logo" class="brandmark-logo logo-text logo-sm-vertical logo-lg-tagline logo-nav-text logo-nav-sm-horizontal text-left text-primary text-nav-light offset-1 offset-sm-0">
+          <div class="logo-wrapper">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/logo-glyph.svg" class="logo-glyph" alt="logo glyph" />
+            <div class="logo-text-group">
+              <h1><span>mikan-megane</span></h1>
+              <h2><span></span></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/blurred-image-1.jpg">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" href="#intro">
+              <i class="now-ui-icons shopping_shop"></i>
+              <p>Home</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" href="#content">
+              <i class="now-ui-icons education_paper"></i>
+              <p>Content</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" href="#guide">
+              <i class="now-ui-icons design_app"></i>
+              <p>Code</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/mikan_megane">
+              <i class="fa fa-twitter"></i>
+              <p class="hidden-lg-up">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="#">
+              <i class="fa fa-facebook-square"></i>
+              <p class="hidden-lg-up">Facebook</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="#">
+              <i class="fa fa-instagram"></i>
+              <p class="hidden-lg-up">Instagram</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="wrapper">
+    <div class="page-header clear-filter bg-dark">
+      <div class="page-header-inner">
+        <a name="intro"></a>
+        <div class="container">
+          <div class="row">
+            <div class="brand brand-left brand-top col-lg-8 offset-lg-0 col-md-12 offset-sm-0 col-10 offset-1">
+              <h1 class="title text-light">Coming Soon</h1>
+              <h5 class="text-light">
+                STAY TUNED!
+              </h5>
+              <p style="text-align: left">
+                <a href="#jp" class="btn btn-info btn-round btn-lg">Learn more</a>
+              </p>
+              <div class="hero-cards-wrapper">
+                <ul class="hero-cards">
+                  <li class="hero-card-3 bg-info" style="border-radius: 10px; transform-origin: 50% 50% 0px; transform: rotateX(65deg) rotateZ(45deg) translateX(-200px) translateY(580px);">
+                    <div class="card-signup" style="font-size: 1.2em; margin-top: 1em">
+                      <form class="form" method="" action="">
+                        <div class="header header-primary text-center">
+                          <h4>Sign Up</h4>
+                          <div class="social-line">
+                            <a href="#" class="btn btn-neutral btn-facebook btn-icon btn-icon-mini">
+                            <i class="fa fa-facebook-square"></i>
+                          </a>
+                            <a href="#" class="btn btn-neutral btn-twitter btn-icon">
+                            <i class="fa fa-twitter"></i>
+                          </a>
+                            <a href="#" class="btn btn-neutral btn-google btn-icon  btn-icon-mini">
+                            <i class="fa fa-google-plus"></i>
+                          </a>
+                          </div>
+                        </div>
+                        <div class="content">
+                          <div class="input-group form-group-no-border">
+                            <span class="input-group-addon">
+                            <i class="now-ui-icons users_circle-08"></i>
+                          </span>
+                            <input type="text" class="form-control" placeholder="First Name...">
+                          </div>
+                          <div class="input-group form-group-no-border">
+                            <span class="input-group-addon">
+                            <i class="now-ui-icons ui-1_email-85"></i>
+                          </span>
+                            <input type="text" class="form-control" placeholder="Email...">
+                          </div>
+                          <div class="input-group form-group-no-border">
+                            <span class="input-group-addon">
+                            <i class="now-ui-icons ui-1_lock-circle-open"></i>
+                          </span>
+                            <input type="password" placeholder="Password..." class="form-control">
+                          </div>
+                        </div>
+                        <div class="footer text-center">
+                          <a href="#" class="btn btn-dark btn-round btn-lg">Get Started</a>
+                        </div>
+                      </form>
+                    </div>
+                  </li>
+                  <li class="hero-card-2 bg-light" style="border-radius: 10px; transform-origin: 50% 50% 0px; transform: rotateX(65deg) rotateZ(45deg) translateX(-200px) translateY(-720px); opacity: 0.9">
+                    <div class="row">
+                      <div class="col-6 bg-light">
+                        <div class="caption caption-left text-center bg-light">
+                          <h1><a href="#"><i class="now-ui-icons location_map-big"></i></a></h1>
+                          <h4><a href="#">The stars</a></h4>
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="fill-image">
+                          <div class="header-image unsplash" style="background-image: url('assets/img/camera1.jpg');"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row clear-padding">
+                      <div class="col-6 bg-accent">
+                        <div class="caption caption-left text-center bg-accent text-light">
+                          <h1><a href="#"><i class="now-ui-icons location_pin text-light"></i></a></h1>
+                          <h4><a href="#" class="text-light">Destination</a></h4>
+                        </div>
+                      </div>
+                      <div class="col-6">
+                        <div class="fill-image">
+                          <div class="header-image unsplash" style="background-image: url('assets/img/camera4.jpg');"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="hero-card-1 bg-primary" style="border-radius: 10px; transform-origin: 50% 50% 0px; transform: rotateX(65deg) rotateZ(45deg) translateX(-60px) translateY(60px);">
+                    <div class="brandmark-logo logo-vertical logo-tagline text-light">
+                      <div class="logo-wrapper">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/logo-glyph.svg" class="logo-glyph" alt="logo glyph" />
+                        <div class="logo-text-group">
+                          <h1 class="main_logo_text font_title"><span class="company_name">mikan-megane</span></h1>
+                          <h2 class="font_subtitle maincolor_secondary"><span class="tagline"></span></h2>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="main" id="jp">
+      <div class="page-header clear-filter" filter-color="dark">
+        <div class="header-image unsplash" style="background-image: url('assets/img/camera1.jpg');"> </div>
+        <div class="container">
+          <a name="hero"></a>
+          <div>
+            <h2 class="top-buffer title text-primary">作成中・・・</h2>
+            <p>
+              新しい"みかんのめがね"を作っています<br />日本語になると急にチープになる・・・<br />どうしよう・・・
+            </p>
+            <p>
+              <a href="http://tumblr.mikan-megane.com/" class="btn btn-primary btn-round btn-lg">Tumblrはこちら</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<!--   Core JS Files   -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/core/tether.min.js" type="text/javascript"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/plugins/bootstrap-switch.js"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+<!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/now-ui-kit.js" type="text/javascript"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/svg-injector.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+  $(document).ready(function () {
+    // the body of this function is in assets/js/now-ui-kit.js
+    nowuiKit.initSliders();
+  });
+
+  SVGInjector(document.querySelectorAll('img.logo-glyph'));
+
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72260865-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</html>
